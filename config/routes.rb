94042922cc954e8be07417515cabc76e9a72ea53
egong <egong82@gmail.com>
2013-08-28 11:37:16 -0400

    pull request

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 CapybaraApp::Application.routes.draw do
 
+  root to: "welcome#index"
+
+    get "/javascript" => "welcome#javascript"
+    get "/quizzes" => "welcome#quizzes"
+
+  resources :welcome
+
 
 end
