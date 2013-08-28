fd02095a654c3f00c3af56a2a898aadc6b317037
Zachariah Reitano <zreitano@gmail.com>
2013-08-28 11:21:07 -0400

    Done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,41 @@
 //
 //= require jquery
 //= require jquery_ujs
+var counter = 0;
+Array.prototype.clone = function() {
+  return this.slice(0);
+};
+$(function(){
+  $('#click-me').click(function(){
+    console.log("clicked");
+    $('#comp-sci').html('Comp-Sigh');
+    $('body').addClass('pink').css("background-color", "pink");
+    $('#click-me').attr("disabled", true);
+  });
+
+  $('#calc-button').click(function(){
+    console.log("clicked");
+    var numbers_string = $('#input-value').val();
+    var numbers_array = numbers_string.split(',');
+    for(var i=0; i < numbers_array.length; i++) {
+      numbers_array[i] = +numbers_array[i];
+    }
+    var j = 0;
+    var clone = numbers_array.slice(0);
+    var sorted = numbers_array.sort();
+    for(j = 0; j < numbers_array.length; j ++ ) {
+      if (clone[j] === sorted[j]) {
+        counter += 1;
+      }
+    }
+    if(counter === numbers_array.length) {
+      $('body').css("background-color", "green");
+    } else {
+      $('body').css("background-color", "red");
+    }
+    $('#input-value').val("");
+    counter = 0;
+  });
+
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 CapybaraApp::Application.routes.draw do
 
+root to: 'welcome#index'
+get '/javascript' => 'welcome#javascript'
+get '/quizzes' => 'welcome#quizzes'
 
 end
