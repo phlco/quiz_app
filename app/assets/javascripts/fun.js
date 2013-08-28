fc0c2f3bb99ade6503a8247dfff71cceff69bdae
Christopher Wright <cmwright33@gmail.com>
2013-08-28 11:24:42 -0400

    11 tests passing

---
+++ app/assets/javascripts/fun.js
@@ -0,0 +1,27 @@
+
+
+
+
+$(function() {
+
+  $('#funky').on('click', function(){
+    $('h1').text('Comp-Sigh');
+    $('body').addClass("pink");
+    $('#funky').attr("disabled", true);
+  });
+
+  $('#awesome-button').on('click', function(){
+
+
+  });
+
+});
+
+
+cool = {
+
+   getNumbers: function() {
+    $('#maths').val();
+   }
+
+};
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -19,67 +19,93 @@ describe "The Home Page" do
     visit '/'
   end
 
-  # it "says 'hello and welcome'" do
-  #   expect(page).to have_content("hello and welcome")
-  # end
+  it "says 'hello and welcome'" do
+    expect(page).to have_content("hello and welcome")
+  end
 
-  # it "has the title 'Week 1011'" do
-  #   expect(page).to have_title "Week 1011"
-  # end
+  it "has the title 'Week 1011'" do
+    expect(page).to have_title "Week 1011"
+  end
 
-  # it "has a link to /javascript" do
-  #   find_link('JavaScript').click
-  #   expect(page).to have_content('Comp-Sci')
-  #   expect(current_path).to eq('/javascript')
-  # end
+  it "has a link to /javascript" do
+    find_link('JavaScript').click
+    expect(page).to have_content('Comp-Sci')
+    expect(current_path).to eq('/javascript')
+  end
 
+  it "has a link to /quizzes" do
+    find_link('quizzes').click
+    expect(current_path).to eq('/quizzes')
+  end
 end
 
 describe 'A page called JavaScript' do
 
-  # before(:each) do
-  #   visit '/javascript'
-  # end
+  before(:each) do
+    visit '/javascript'
+  end
 
-  # it "Has an h1 with the text 'Comp-Sci'" do
-  #   h1 = page.find('h1')
-  #   text = 'Comp-Sci'
-  #   expect(h1.has_content?(text)).to be true
-  # end
+  it "Has an h1 with the text 'Comp-Sci'" do
+    h1 = page.find('h1')
+    text = 'Comp-Sci'
+    expect(h1.has_content?(text)).to be true
+  end
 
-  # it "has a button labeled 'Click Me!'" do
-  #   button = page.find('button')
-  #   label = 'Click Me!'
-  #   expect(button.has_content?(label)).to be true
-  # end
+  it "has a button labeled 'Click Me!'" do
+    button = page.find('button')
+    label = 'Click Me!'
+    expect(button.has_content?(label)).to be true
+  end
 
-  # describe "clicking the button with JavaScript enabled", :js => true do
+  describe "clicking the button with JavaScript enabled", :js => true do
 
-  #   before(:each) do
-  #     click_button('Click Me!');
-  #   end
+    before(:each) do
+      click_button('Click Me!');
+    end
 
-  #   it "changes the h1's text to Comp-Sigh using jQuery" do
-  #     expect(page).to have_content('Comp-Sigh')
-  #     expect(page).to have_no_content('Comp-Sci')
-  #   end
+    it "changes the h1's text to Comp-Sigh using jQuery" do
+      expect(page).to have_content('Comp-Sigh')
+      expect(page).to have_no_content('Comp-Sci')
+    end
 
-  #   it "changes the body's background to pink", :js => true do
-  #     expect(page).to have_css('body.pink')
-  #   end
+    it "changes the body's background to pink", :js => true do
+      expect(page).to have_css('body.pink')
+    end
 
-  #   it "disables the button", :js => true do
-  #     button = page.find('button')
-  #     expect(button.disabled?).to be true
-  #   end
+    it "disables the button", :js => true do
+      button = page.find('button')
+      expect(button.disabled?).to be true
+    end
 
-  # end
-
+  end
 end
+
+  describe "A page called quizzes" do
+
+    before(:each) do
+      visit '/quizzes'
+    end
+
+    it "has a button labeled 'calc'" do
+      button = page.find('button')
+      label = 'calc'
+      expect(button.has_content?(label)).to be true
+    end
+
+    it 'has a field with id called mathes' do
+      id = '#maths'
+      expect(page.has_css?(id)).to be true
+    end
+
+  end
+
 
 # MORE?!
 
 # There should be a link on the home page called "quizzes"
+
+
+
 # When a user clicks on "quizzes" they should be re-directed to "/quizzes"
 # "/quizzes" should have an input field and a button labeled 'calc'
 # "/quizzes" should not have a form tag
